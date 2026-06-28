12253194c257e52f201c03bdfd9d7b062d772b72
ddrapps <tm2z9n4b74@privaterelay.appleid.com>
2026-06-27 21:49:55 -0600

    Update index.ts

--- apps/tellava-backend/src/index.ts
+++ apps/tellava-backend/src/index.ts
@@ -7,7 +7,7 @@ import nearbyStoresRouter from './routes/nearbyStores';
 
 const app = express();
 
-app.use(cors({ origin: env.appOrigin === '*' ? true : env.appOrigin }));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
